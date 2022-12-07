0d561b57726a85596979534929134d6b2187d3a5
ijaz bacha <ijazbacha14@gmail.com>
2022-12-07 10:26:26 +0500

    Design exercise sections

--- WorkOut/MVVM/Models/Exercise.cs
+++ WorkOut/MVVM/Models/Exercise.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace WorkOut.MVVM.Models
 {
+    [AddINotifyPropertyChangedInterface]
     public class Exercise
     {
         public string Name { get; set; }
